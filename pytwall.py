300b2374bf1c411e1e7d4f3df8839a989e75d73b
Martin Tomes <tomesm@gmail.com>
2017-03-17 21:13:08 +0100

    Add infinite loop to check new tweets.

--- pytwall.py
+++ pytwall.py
@@ -1,7 +1,9 @@
+import configparser
+import sys
 import requests
 import base64
 
-def get_tw_session(api_key, api_secret):
+def get_session(api_key, api_secret):
     session = requests.Session()
     secret = '{}:{}'.format(api_key, api_secret)
     secret64 = base64.b64encode(secret.encode('ascii')).decode('ascii')
@@ -24,3 +26,12 @@ def get_tw_session(api_key, api_secret):
     session.auth = bearer_auth
 
     return session
+
+
+def get_credentials(file):
+    ''' '''
+    config = configparser.ConfigParser()
+    config.read(file)
+
+    return config['twitter']['api_key'], config['twitter']['api_secret']
+
