79ae9cc2a3be373a96c770151bd0e33cdea47202
Martin Tomes <tomesm@gmail.com>
2017-04-04 14:06:35 +0200

    Add setup file for build a package.

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+with open('README') as f:
+    long_description = ''.join(f.readlines())
+
+
+setup(
+    name='tomesm_pytwall',
+    version='0.1',
+    description='Python Twiter Wall',
+    long_description='An application using the Twitter REST API and OAuth showing tweets based on a given query.',
+    author='Martin Tomes',
+    author_email='tomesm@gmail.com',
+    keywords='twitter,wall',
+    license='Public Domain',
+    url='https://github.com/tomesm/pytwall',
+    packages=find_packages(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries',
+        ],
+    zip_safe=False,
+    install_requires=['Flask', 'jinja2', 'click>=6'],
+    package_data={
+        'pytwal': ['templates/*.html'],
+    }
+)
